e12054593e75d566f02a58129f477d59b4ba462d
红色枫叶 <BlackMaple@live.cn>
2026-06-01 13:45:14 +0800

    1.update package

--- Maple.ImGui.Backends.Test/WindowsFormsLifetime.cs
+++ Maple.ImGui.Backends.Test/WindowsFormsLifetime.cs
@@ -1,21 +1,27 @@
 using ImGui.App.D3D11;
+using Maple.ImGui.Backends.Windows.ImGuiCore;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
-public class WindowsFormsLifetime<TWindow>(IHostApplicationLifetime hostLifetime, IServiceProvider services) : BackgroundService
+public class WindowsFormsLifetime<TWindow>(IHostApplicationLifetime hostLifetime, IServiceProvider services)
+    : BackgroundService
    where TWindow : ITestWindow
 {
     private readonly IHostApplicationLifetime _hostLifetime = hostLifetime;
     private readonly IServiceProvider _services = services;
 
 
-    protected override Task ExecuteAsync(CancellationToken stoppingToken)
+
+
+    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
     {
-        return Task.Run(() =>
+        var win32ImGuiBackendService = _services.GetRequiredService<Win32ImGuiBackendService>();
+        await win32ImGuiBackendService.StartAsync(stoppingToken).ConfigureAwait(false);
+        await Task.Run(() =>
         {
             TWindow.Run();
             this._hostLifetime.StopApplication();
-        }, stoppingToken);
+        }, stoppingToken).ConfigureAwait(false);
 
     }
 }
